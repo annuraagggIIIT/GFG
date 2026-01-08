f01770070afc950fd314e63ee17a69f78df601e3
Anurag Khanna <99660777+annuraagggIIIT@users.noreply.github.com>
2026-01-09 04:15:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Preorder Traversal/preorder-traversal.cpp
+++ Difficulty: Basic/Preorder Traversal/preorder-traversal.cpp
@@ -13,18 +13,17 @@ class Node
 */
 
 class Solution {
-public:
-    void solve(Node* root, vector<int>& ans) {
-        if (root == NULL) return;
-
-        ans.push_back(root->data);
-        solve(root->left, ans);
-        solve(root->right, ans);
-    }
-
+  public:
+  void solve(Node* root , vector<int>& ans) {
+      if (root==NULL)return ;
+      ans.push_back(root->data);
+      solve(root->left,ans);
+      solve(root->right,ans);
+  }
     vector<int> preOrder(Node* root) {
-        vector<int> ans;
-        solve(root, ans);
-        return ans;
+        // code here
+        vector<int>ans;
+        solve(root,ans);
+        return  ans;
     }
 };
